1f473f9e494f7923494840633f820c38c405f23d
checongbinh20001990 <checongbinh20011990@gmail.com>
2023-03-03 20:44:43 +0700

    upload file

---
+++ src/main/java/com/cybersoft/osahaneat/controller/MenuController.java
@@ -0,0 +1,45 @@
+package com.cybersoft.osahaneat.controller;
+
+import com.cybersoft.osahaneat.payload.ResponseData;
+import com.cybersoft.osahaneat.service.imp.FileStorageServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+
+    @Autowired
+    FileStorageServiceImp fileStorageServiceImp;
+
+    //Ở API add menu cho phép người dùng tạo ra một menu mới và upload hình ảnh của menu này
+    //Lấy danh sách menu và hình ảnh của menu
+    //Redis
+
+    //form-data : stream
+    //request body : Chuyển file về base64 -> sẽ bị x1.5 dung lượng file
+    //Lưu ở database : file phải chuyển về base64, file phải là dạng byte -> Không khuyến khích
+    //Lưu ở ổ đĩa : file upload sẽ được lưu vào ổ đĩa cứng -> Luôn sử dụng
+    @PostMapping("")
+    public ResponseEntity<?> addMenu(MultipartFile file){
+        ResponseData responseData = new ResponseData();
+        boolean isSuccess = fileStorageServiceImp.saveFiles(file);
+        responseData.setData(isSuccess);
+
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
+    }
+
+    @GetMapping("/files/{filesname:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filesname){
+        Resource resource = fileStorageServiceImp.load(filesname);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"" + filesname + "\"")
+                .body(resource);
+    }
+
+}
--- src/main/java/com/cybersoft/osahaneat/security/CustomConfigSecurity.java
+++ src/main/java/com/cybersoft/osahaneat/security/CustomConfigSecurity.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,40 +17,53 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CustomConfigSecurity {
 
+    @Autowired
+    CustomAuthentication authProvider;
+
+    @Autowired
+    CustomFilterJwt customFilterJwt;
+
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails admin = User.withUsername("hach")
-                .password(encoder.encode("hacheery"))
-                .roles("ADMIN")
-                .build();
-        UserDetails user = User.withUsername("user")
-                .password(encoder.encode("pwd1"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(authProvider);
+        return authenticationManagerBuilder.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+//    Author : Có quyền truy cập một link cụ thể nào đó không
+//    Role : role thì lúc tạo user thì phải sử dụng .roles và giá trị của roles phải
+//    luôn luôn có prefix ROLE_têngìcũngđược
+//    - thường mô tả cho việc có quyền để làm một chức năng nào đó ví dụ : thêm, xóa
+//    sửa....
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                     .antMatchers("/login/signin")
                     .permitAll()
+//                    .antMatchers("/login/signup")
+//                    .hasAnyRole("ROLE_ADMIN","ROLE_USER")
+//                    .hasAuthority("ADMIN")
+                    .antMatchers("/menu/files/**")
+                    .permitAll()
                     .anyRequest()
-                    .authenticated()
-                .and().httpBasic();
+                    .authenticated();
 
-
+        http.addFilterBefore(customFilterJwt, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
